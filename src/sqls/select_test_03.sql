1ec9b46c6ec0570905021acf53dfbfc39fcaece6
Jingwen Xia <amyfor7@gmail.com>
2022-10-28 13:32:17 +0800

    [Jingwen Xia] feat: complete select 03

--- src/sqls/select_test_03.sql
+++ src/sqls/select_test_03.sql
@@ -2,3 +2,6 @@
  * 请告诉我员工（employee）中 jobTitle 不是 'Sales Rep' 的员工的 officeCode（不重复）。结果
  * 仅仅包含 officeCode 即可，并按照 officeCode 排序。
  */
+SELECT distinct `officeCode`
+FROM `employees`
+WHERE `jobTitle` != 'Sales Rep';
