c2e689814dd577580742a45fbb94207009372e06
Jingwen Xia <amyfor7@gmail.com>
2022-10-29 14:08:29 +0800

    [Jingwen Xia] feat: complete group by 04

--- src/sqls/groupby_test_04.sql
+++ src/sqls/groupby_test_04.sql
@@ -7,3 +7,8 @@
  *
  * 其结果应当以 `orderNumber` 排序。
  */
+SELECT `orderNumber`, SUM(`quantityOrdered` * `priceEach`) as `totalPrice`
+FROM `orderdetails`
+GROUP BY `orderNumber`
+HAVING `totalPrice` > 60000
+ORDER BY `orderNumber`;
