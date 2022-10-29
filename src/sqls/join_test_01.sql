4573192bc8fd5cf79d5acb89de90d2e618c2af8b
Jingwen Xia <amyfor7@gmail.com>
2022-10-29 14:18:14 +0800

    [Jingwen Xia] feat: complete join test 01

--- src/sqls/join_test_01.sql
+++ src/sqls/join_test_01.sql
@@ -9,3 +9,10 @@
  * firstName 和 lastName 是员工的姓名，city 是办公室所在的城市。结果按照员工的 `employeeNumber`
  * 排序。
  */
+
+SELECT `firstName`, `lastName`, `city`
+FROM `employees`
+INNER JOIN `offices`
+ON employees.`officeCode` = offices.`officeCode`
+WHERE `country` = 'France'
+ORDER BY `employeeNumber`;
